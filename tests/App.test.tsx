d2ca6511e01b7175f02fb6f89571cb97f7e4616f
Minh Nguyet Nguyen <nguyenmnt@vcu.edu>
2019-12-13 09:25:05 -0500

    adding config for unit testing

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { Text } from 'react-native';
 import { expect } from 'chai';
 
-import App from '../App';
+import App from '../components/App';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App>', () => {
   let wrapper;
